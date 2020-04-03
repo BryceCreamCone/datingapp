769684c92da1f30c828bacdfa15d67888d2cb895
Bryce <bgust9@gmail.com>
2020-04-03 14:28:06 -0500

    Added Login to AuthController

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
   public class UserForRegisterDto
   {
+    [Required]
     public string Username { get; set; }
+    [Required]
+    [StringLength(50, MinimumLength = 8, ErrorMessage = "Your password must be at least 8 characters long (Maximum length is 50)")]
     public string Password { get; set; }
   }
 }
