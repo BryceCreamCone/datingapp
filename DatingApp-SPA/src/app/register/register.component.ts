2d1fd9b94f588595d5cfae01044161354044b361
Bryce <bgust9@gmail.com>
2020-04-05 15:19:29 -0500

    Added 3rd party components to style alerts and bootstrap, and also ngx-bootstrap

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,26 +11,25 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('Registration successful');
+      this.alertify.success('Registration successful');
       this.authService.login(this.model).subscribe(next => {
-        console.log('Automatically Logged In');
+        this.alertify.success('Automatically Logged In');
       }, error => {
-        console.log(error);
+        this.alertify.error(error);
       });
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('Cancelled');
   }
 }
