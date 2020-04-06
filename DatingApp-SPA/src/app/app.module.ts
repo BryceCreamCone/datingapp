7b2bdd48adb30db246f11b99333844b6fa36a842
Bryce <bgust9@gmail.com>
2020-04-06 13:14:49 -0500

    Added MemberCard Component and interfaces for Models

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -11,10 +11,11 @@ import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 
 @NgModule({
@@ -24,6 +25,7 @@ import { appRoutes } from './routes';
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
+    MemberCardComponent,
     ListsComponent,
     MessagesComponent
   ],
