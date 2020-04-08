842d028d9d88c6a1eba91d46d756acdb7a75e409
Bryce <bgust9@gmail.com>
2020-04-08 12:42:01 -0500

    Added Configuration for Cloudinary

---
+++ DatingApp.API/Migrations/20200408173935_isMainToIsMain.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class isMainToIsMain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isMain",
+                table: "Photos",
+                newName: "IsMain");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMain",
+                table: "Photos",
+                newName: "isMain");
+        }
+    }
+}
