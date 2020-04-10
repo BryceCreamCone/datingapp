516e90b9f86ffa96ca4c72ca160d44af79c5b3cb
Bryce <bgust9@gmail.com>
2020-04-09 23:12:50 -0500

    Added Likes to userParams

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -57,6 +57,17 @@ namespace DatingApp.API.Data
         .OrderByDescending(u => u.LastActive)
         .AsQueryable();
 
+      if (userParams.Likers)
+      {
+        var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+        users = users.Where(u => userLikers.Contains(u.Id));
+      }
+      else
+      {
+        var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+        users = users.Where(u => userLikees.Contains(u.Id));
+      }
+
       if (userParams.MinAge != 18 || userParams.MaxAge != 120)
       {
         var minDob = System.DateTime.Today.AddYears(-userParams.MaxAge - 1);
@@ -77,6 +88,17 @@ namespace DatingApp.API.Data
         }
       }
       return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+    }
+
+    private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+    {
+      var user = await _context.Users
+        .Include(x => x.Likers)
+        .Include(x => x.Likees)
+        .FirstOrDefaultAsync(u => u.Id == id);
+
+      if (likers) return user.Likers.Where(u => u.LikeeId == id).Select(i => i.LikerId);
+      return user.Likees.Where(u => u.LikerId == id).Select(i => i.LikeeId);
     }
 
     public async Task<bool> SaveAll()
