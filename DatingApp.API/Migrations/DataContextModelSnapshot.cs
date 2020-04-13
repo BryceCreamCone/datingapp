0cedf303523ae53b6fce344a86697da2a89ac917
Bryce <bgust9@gmail.com>
2020-04-13 12:29:02 -0500

    Merged with remote origin

--- DatingApp.API/Migrations/DataContextModelSnapshot.cs
+++ DatingApp.API/Migrations/DataContextModelSnapshot.cs
@@ -7,189 +7,190 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.API.Migrations
 {
-    [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+  [DbContext(typeof(DataContext))]
+  partial class DataContextModelSnapshot : ModelSnapshot
+  {
+    protected override void BuildModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.3");
+      modelBuilder
+          .HasAnnotation("ProductVersion", "3.1.3")
+          .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
-            modelBuilder.Entity("DatingApp.API.Models.Like", b =>
-                {
-                    b.Property<int>("LikerId")
-                        .HasColumnType("INTEGER");
+      modelBuilder.Entity("DatingApp.API.Models.Like", b =>
+          {
+            b.Property<int>("LikerId")
+                      .HasColumnType("int");
 
-                    b.Property<int>("LikeeId")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("LikeeId")
+                      .HasColumnType("int");
 
-                    b.HasKey("LikerId", "LikeeId");
+            b.HasKey("LikerId", "LikeeId");
 
-                    b.HasIndex("LikeeId");
+            b.HasIndex("LikeeId");
 
-                    b.ToTable("Likes");
-                });
+            b.ToTable("Likes");
+          });
 
-            modelBuilder.Entity("DatingApp.API.Models.Message", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+      modelBuilder.Entity("DatingApp.API.Models.Message", b =>
+          {
+            b.Property<int>("Id")
+                      .ValueGeneratedOnAdd()
+                      .HasColumnType("int");
 
-                    b.Property<string>("Content")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Content")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<DateTime?>("DateRead")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime?>("DateRead")
+                      .HasColumnType("datetime(6)");
 
-                    b.Property<bool>("IsRead")
-                        .HasColumnType("INTEGER");
+            b.Property<bool>("IsRead")
+                      .HasColumnType("tinyint(1)");
 
-                    b.Property<DateTime>("MessageSent")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("MessageSent")
+                      .HasColumnType("datetime(6)");
 
-                    b.Property<bool>("RecipientDeleted")
-                        .HasColumnType("INTEGER");
+            b.Property<bool>("RecipientDeleted")
+                      .HasColumnType("tinyint(1)");
 
-                    b.Property<int>("RecipientId")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("RecipientId")
+                      .HasColumnType("int");
 
-                    b.Property<bool>("SenderDeleted")
-                        .HasColumnType("INTEGER");
+            b.Property<bool>("SenderDeleted")
+                      .HasColumnType("tinyint(1)");
 
-                    b.Property<int>("SenderId")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("SenderId")
+                      .HasColumnType("int");
 
-                    b.HasKey("Id");
+            b.HasKey("Id");
 
-                    b.HasIndex("RecipientId");
+            b.HasIndex("RecipientId");
 
-                    b.HasIndex("SenderId");
+            b.HasIndex("SenderId");
 
-                    b.ToTable("Messages");
-                });
+            b.ToTable("Messages");
+          });
 
-            modelBuilder.Entity("DatingApp.API.Models.Photo", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+      modelBuilder.Entity("DatingApp.API.Models.Photo", b =>
+          {
+            b.Property<int>("Id")
+                      .ValueGeneratedOnAdd()
+                      .HasColumnType("int");
 
-                    b.Property<DateTime>("DateAdded")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("DateAdded")
+                      .HasColumnType("datetime(6)");
 
-                    b.Property<string>("Description")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Description")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<bool>("IsMain")
-                        .HasColumnType("INTEGER");
+            b.Property<bool>("IsMain")
+                      .HasColumnType("tinyint(1)");
 
-                    b.Property<string>("PublicId")
-                        .HasColumnType("TEXT");
+            b.Property<string>("PublicId")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<string>("Url")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Url")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<int>("UserId")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("UserId")
+                      .HasColumnType("int");
 
-                    b.HasKey("Id");
+            b.HasKey("Id");
 
-                    b.HasIndex("UserId");
+            b.HasIndex("UserId");
 
-                    b.ToTable("Photos");
-                });
+            b.ToTable("Photos");
+          });
 
-            modelBuilder.Entity("DatingApp.API.Models.User", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+      modelBuilder.Entity("DatingApp.API.Models.User", b =>
+          {
+            b.Property<int>("Id")
+                      .ValueGeneratedOnAdd()
+                      .HasColumnType("int");
 
-                    b.Property<string>("City")
-                        .HasColumnType("TEXT");
+            b.Property<string>("City")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<string>("Country")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Country")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<DateTime>("Created")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("Created")
+                      .HasColumnType("datetime(6)");
 
-                    b.Property<DateTime>("DateOfBirth")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("DateOfBirth")
+                      .HasColumnType("datetime(6)");
 
-                    b.Property<string>("Gender")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Gender")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<string>("Interests")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Interests")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<string>("Introduction")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Introduction")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<string>("KnownAs")
-                        .HasColumnType("TEXT");
+            b.Property<string>("KnownAs")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<DateTime>("LastActive")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("LastActive")
+                      .HasColumnType("datetime(6)");
 
-                    b.Property<string>("LookingFor")
-                        .HasColumnType("TEXT");
+            b.Property<string>("LookingFor")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.Property<byte[]>("PasswordHash")
-                        .HasColumnType("BLOB");
+            b.Property<byte[]>("PasswordHash")
+                      .HasColumnType("longblob");
 
-                    b.Property<byte[]>("PasswordSalt")
-                        .HasColumnType("BLOB");
+            b.Property<byte[]>("PasswordSalt")
+                      .HasColumnType("longblob");
 
-                    b.Property<string>("Username")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Username")
+                      .HasColumnType("longtext CHARACTER SET utf8mb4");
 
-                    b.HasKey("Id");
+            b.HasKey("Id");
 
-                    b.ToTable("Users");
-                });
+            b.ToTable("Users");
+          });
 
-            modelBuilder.Entity("DatingApp.API.Models.Like", b =>
-                {
-                    b.HasOne("DatingApp.API.Models.User", "Likee")
-                        .WithMany("Likers")
-                        .HasForeignKey("LikeeId")
-                        .OnDelete(DeleteBehavior.Restrict)
-                        .IsRequired();
+      modelBuilder.Entity("DatingApp.API.Models.Like", b =>
+          {
+            b.HasOne("DatingApp.API.Models.User", "Likee")
+                      .WithMany("Likers")
+                      .HasForeignKey("LikeeId")
+                      .OnDelete(DeleteBehavior.Restrict)
+                      .IsRequired();
 
-                    b.HasOne("DatingApp.API.Models.User", "Liker")
-                        .WithMany("Likees")
-                        .HasForeignKey("LikerId")
-                        .OnDelete(DeleteBehavior.Restrict)
-                        .IsRequired();
-                });
+            b.HasOne("DatingApp.API.Models.User", "Liker")
+                      .WithMany("Likees")
+                      .HasForeignKey("LikerId")
+                      .OnDelete(DeleteBehavior.Restrict)
+                      .IsRequired();
+          });
 
-            modelBuilder.Entity("DatingApp.API.Models.Message", b =>
-                {
-                    b.HasOne("DatingApp.API.Models.User", "Recipient")
-                        .WithMany("MessagesRecieved")
-                        .HasForeignKey("RecipientId")
-                        .OnDelete(DeleteBehavior.Restrict)
-                        .IsRequired();
+      modelBuilder.Entity("DatingApp.API.Models.Message", b =>
+          {
+            b.HasOne("DatingApp.API.Models.User", "Recipient")
+                      .WithMany("MessagesRecieved")
+                      .HasForeignKey("RecipientId")
+                      .OnDelete(DeleteBehavior.Restrict)
+                      .IsRequired();
 
-                    b.HasOne("DatingApp.API.Models.User", "Sender")
-                        .WithMany("MessagesSent")
-                        .HasForeignKey("SenderId")
-                        .OnDelete(DeleteBehavior.Restrict)
-                        .IsRequired();
-                });
+            b.HasOne("DatingApp.API.Models.User", "Sender")
+                      .WithMany("MessagesSent")
+                      .HasForeignKey("SenderId")
+                      .OnDelete(DeleteBehavior.Restrict)
+                      .IsRequired();
+          });
 
-            modelBuilder.Entity("DatingApp.API.Models.Photo", b =>
-                {
-                    b.HasOne("DatingApp.API.Models.User", "User")
-                        .WithMany("Photos")
-                        .HasForeignKey("UserId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-                });
+      modelBuilder.Entity("DatingApp.API.Models.Photo", b =>
+          {
+            b.HasOne("DatingApp.API.Models.User", "User")
+                      .WithMany("Photos")
+                      .HasForeignKey("UserId")
+                      .OnDelete(DeleteBehavior.Cascade)
+                      .IsRequired();
+          });
 #pragma warning restore 612, 618
-        }
     }
+  }
 }
